32532f45c646858ade5d7e594a67f5406bc05d18
kti0423-png <kti0423@gmail.com>
2025-08-13 17:50:01 +0900

    Create 37 조건에 맞는 도서와 저자 리스트 출력하기.sql

---
+++ Lv.3/37 조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,9 @@
+select
+    b.book_id,
+    a.author_name,
+    date_format(b.published_date,'%Y-%m-%d') as PUBLISHED_DATE
+from book b
+inner join author a
+on b.author_id=a.author_id
+where b.category = '경제'
+order by 3 asc;
