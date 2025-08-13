14b7338fe1b0666c8fdf48451a70c62240becb23
kti0423-png <kti0423@gmail.com>
2025-08-13 20:05:53 +0900

    Create 49 식품분류별 가장 비싼 식품의 정보 조회하기.sql

---
+++ Lv.3/49 식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT
+    category,
+    price as max_price,
+    product_name
+from food_product
+where
+    price in (select max(price)
+            from food_product
+            group by category
+            )
+    and category in ('과자', '국', '김치', '식용유')
+order by 2 desc;
