4175d424119a63d63eb2b0ed983e0417da4faab9
kti0423-png <kti0423@gmail.com>
2025-08-13 20:05:45 +0900

    Create 44 가격대 별 상품 개수 구하기.sql

---
+++ Lv.3/44 가격대 별 상품 개수 구하기.sql
@@ -0,0 +1,22 @@
+select
+    PRICE_GROUP,
+    count(PRICE_GROUP) as PRODUCTS
+from
+(
+select
+    case
+        when price < 10000 then 0
+        when price between 10000 and 19999 then 10000
+        when price between 20000 and 29999 then 20000
+        when price between 30000 and 39999 then 30000
+        when price between 40000 and 49999 then 40000
+        when price between 50000 and 59999 then 50000
+        when price between 60000 and 69999 then 60000
+        when price between 70000 and 79999 then 70000
+        when price between 80000 and 89999 then 80000
+        when price between 90000 and 99999 then 90000
+        end as PRICE_GROUP
+from product
+) a
+group by 1
+order by 1 asc;
