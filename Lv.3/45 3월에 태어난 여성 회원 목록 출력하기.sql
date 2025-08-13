4e9834ed96b1d35e74a05756d28cdd39705ce524
kti0423-png <kti0423@gmail.com>
2025-08-13 20:05:47 +0900

    Create 45 3월에 태어난 여성 회원 목록 출력하기.sql

---
+++ Lv.3/45 3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,11 @@
+select
+    member_id,
+    member_name,
+    gender,
+    date_format(date_of_birth, '%Y-%m-%d') as date_of_birth
+from member_profile
+where
+    date_of_birth like ('%-03-%')
+    and gender = 'W'
+    and TLNO is not null
+order by member_id asc;
