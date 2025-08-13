c044fa6a01e4f298d0119a86cecdc61ed19c01d4
kti0423-png <kti0423@gmail.com>
2025-08-13 15:20:55 +0900

    Create 30 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql

---
+++ Lv.2/30 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -0,0 +1,10 @@
+select
+	car_type,
+	count(1) as cars
+from car_rental_company_car
+where
+	options like ('%통풍시트%')
+	or options like ('%열선시트%')
+	or options like ('%가죽시트%')
+group by 1
+order by 1 asc;
