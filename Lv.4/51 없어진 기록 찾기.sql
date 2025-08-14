bb92e31f4181bbf72631d4ac496aacda1f80b7f0
kti0423-png <kti0423@gmail.com>
2025-08-14 10:27:05 +0900

    Create 51 없어진 기록 찾기.sql

---
+++ Lv.4/51 없어진 기록 찾기.sql
@@ -0,0 +1,10 @@
+SELECT
+    o.animal_id,
+    o.name
+from animal_ins i
+right join animal_outs o
+on i.animal_id=o.animal_id
+where
+    o.animal_id is not null
+    and i.animal_id is null
+order by o.animal_id
