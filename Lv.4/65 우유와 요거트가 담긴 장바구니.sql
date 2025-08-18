0bec03f021927382ed745491134cbae108485d01
kti0423-png <kti0423@gmail.com>
2025-08-18 17:54:44 +0900

    Update 65 우유와 요거트가 담긴 장바구니.sql

--- Lv.4/65 우유와 요거트가 담긴 장바구니.sql
+++ Lv.4/65 우유와 요거트가 담긴 장바구니.sql
@@ -2,19 +2,17 @@ select
     a.cart_id
 from
 (
-select
-    cart_id,
-    name
+select cart_id, name
 from cart_products
-where name = 'Milk'
-) as a
+where
+    name = 'Milk'
+) a
 inner join
 (
-select
-    cart_id,
-    name
+select cart_id, name
 from cart_products
-where name = 'Yogurt'
-) as b
+where
+    name = 'Yogurt'
+) b
 on a.cart_id=b.cart_id
 order by 1
