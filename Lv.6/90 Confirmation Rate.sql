e59658c57a26cc8d92793c6843e01ac120bb4f68
kti0423-png <kti0423@gmail.com>
2025-08-26 11:57:14 +0900

    Delete 90 Confirmation Rate.sql

--- Lv.6/90 Confirmation Rate.sql
+++
@@ -1,8 +0,0 @@
-select
-    s.user_id,
-    Round(avg(case
-                when c.action = 'confirmed' then 1.00
-                else 0 end),2) as confirmation_rate
-from signups s
-left join confirmations c on s.user_id=c.user_id
-group by 1
