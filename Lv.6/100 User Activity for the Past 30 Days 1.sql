7cbf28e44cf7956bc0d2c7ea4efbb35e87d9e522
kti0423-png <kti0423@gmail.com>
2025-08-26 12:04:57 +0900

    Update 100 User Activity for the Past 30 Days 1.sql

--- Lv.6/100 User Activity for the Past 30 Days 1.sql
+++ Lv.6/100 User Activity for the Past 30 Days 1.sql
@@ -2,5 +2,5 @@ select
     activity_date as day,
     count(distinct user_id) as active_users
 from activity
-where activity_date between '2019-06-28' and '2019-07-27'
+where activity_date between date_sub('2019-07-27', interval 30 day)+1 and '2019-07-27'
 group by 1
