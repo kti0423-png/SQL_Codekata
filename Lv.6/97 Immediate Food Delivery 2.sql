54c9b3b430675d867351b896d15a42f09491c3c5
kti0423-png <kti0423@gmail.com>
2025-08-26 11:52:29 +0900

    Create 97 Immediate Food Delivery 2.sql

---
+++ Lv.6/97 Immediate Food Delivery 2.sql
@@ -0,0 +1,7 @@
+select
+    Round(sum(order_date = customer_pref_delivery_date)/count(distinct customer_id)*100,2) as immediate_percentage
+from delivery
+where (customer_id, order_date) in (
+    select customer_id, min(order_date)
+    from delivery
+    group by customer_id)
