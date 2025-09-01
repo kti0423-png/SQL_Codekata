3c0cfe0c4d50a49ed78403e50189397c47ba16d0
kti0423-png <kti0423@gmail.com>
2025-09-01 15:51:12 +0900

    Create 116 Restaurant Growth.sql

---
+++ Lv.6/116 Restaurant Growth.sql
@@ -0,0 +1,19 @@
+select
+    visited_on,
+    (
+        select sum(amount)
+        from customer
+        where visited_on between date_sub(c.visited_on, interval 6 day) and c.visited_on
+    ) as amount,
+    (
+        select Round(sum(amount)/7,2)
+        from customer
+        where visited_on between date_sub(c.visited_on, interval 6 day) and c.visited_on
+    ) as average_amount
+from customer as c
+where
+    visited_on >= (
+        select date_add(min(visited_on), interval 6 day)
+        from customer
+        )
+group by visited_on
