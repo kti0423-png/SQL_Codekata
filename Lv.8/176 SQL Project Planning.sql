ffcae0a728bba5162ca13f18eaecd17265f4a024
kti0423-png <kti0423@gmail.com>
2025-09-23 17:10:03 +0900

    Create 176 SQL Project Planning.sql

---
+++ Lv.8/176 SQL Project Planning.sql
@@ -0,0 +1,15 @@
+select a.start_date, min(b.end_date)
+from 
+    (select start_date
+    from projects
+    where start_date not in (
+        select end_date
+        from projects)) a,
+    (select end_date
+    from projects
+    where end_date not in (
+        select start_date
+        from projects)) b
+where start_date < end_date
+group by start_date
+order by datediff(min(end_date),start_date) asc, start_date asc
