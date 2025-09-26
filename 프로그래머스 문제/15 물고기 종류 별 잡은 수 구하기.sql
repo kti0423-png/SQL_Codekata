9a413db5e5783096a365390bef3a82079e6f7003
kti0423-png <kti0423@gmail.com>
2025-09-26 11:21:33 +0900

    Create 15 물고기 종류 별 잡은 수 구하기.sql

---
+++ 프로그래머스 문제/15 물고기 종류 별 잡은 수 구하기.sql
@@ -0,0 +1,7 @@
+select
+    count(*) as fish_count,
+    n.fish_name
+from fish_info as i
+inner join fish_name_info as n on i.fish_type = n.fish_type
+group by n.fish_name
+order by fish_count desc;
