115bdf46932cbfcacd9c67c5416f20224982c7a9
kti0423-png <kti0423@gmail.com>
2025-09-26 11:21:31 +0900

    Create 14 노선별 평균 역 사이 거리 조회하기.sql

---
+++ 프로그래머스 문제/14 노선별 평균 역 사이 거리 조회하기.sql
@@ -0,0 +1,7 @@
+select
+    route,
+    concat(round(sum(d_between_dist),1),'km') as TOTAL_DISTANCE,
+    concat(round(avg(d_between_dist),2),'km') as AVERAGE_DISTANCE
+from subway_distance
+group by 1
+order by round(sum(d_between_dist),1) desc;
